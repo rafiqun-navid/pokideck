884e1ebb6e3af2a20df5abfc23d0c2f6738fe3a3
grumpy_slime <rafiqunnabi.navid@gmail.com>
2026-07-19 04:22:52 +0600

    pokemon api and prettier

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,37 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+type Pokemon = {
+  name: string;
+};
 
 export default function Index() {
+  const [data, setData] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    // fetch data from pokimon API
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+      const pokedata = await response.json();
+      setData(pokedata.results);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+      {data.map(item => (
+        <View key={item.name}>
+          <Text>{item.name}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
